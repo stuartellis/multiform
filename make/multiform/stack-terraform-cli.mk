b6b084f33be372715e33fdd8a1a58721b4604d13
Stuart Ellis <stuart@stuartellis.name>
2022-11-10 09:14:54 +0000

    Fix for no stack instance ID

--- make/multiform/stack-terraform-cli.mk
+++ make/multiform/stack-terraform-cli.mk
@@ -1,5 +1,10 @@
 # Multiform: Terraform CLI commands
 
+ifdef STACK_INSTANCE
+	MF_EXTRA_OPTIONS := -i $(STACK_INSTANCE)
+else
+	MF_EXTRA_OPTIONS :=
+endif
+
 .PHONY: stack-fmt
-stack-fmt:
-	@$(shell python $(MF_TF_CMD_BUILDER) fmt -e $(ENVIRONMENT) -i $(STACK_INSTANCE) -s $(STACK_NAME))
+	stack-fmt:@$(shell python $(MF_TF_CMD_BUILDER) fmt -e $(ENVIRONMENT) -s $(MF_STACK_NAME) $(MF_EXTRA_OPTIONS))
